fa27d354221925649031260454d221dc0f1b0480
Himanshu Rastogi <98731354+HimanshuRastogiOfficial@users.noreply.github.com>
2022-09-03 04:13:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,58 @@ Bot = Client(
     api_hash=API_HASH
 )
 
+@Bot.on_message(filters.command('start'))
+async def start(bot, message):
+    if len(message.command) == 1:
+        await message.reply(
+            text=f"Hello {message.from_user.mention}, I am a Powerful File Store Bot devoloped by @Hagadmansa.\n\nJust send me any photo, video, voice, audio, document, sticker, animation or videonote, i'll share you you it's permanent link.",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton('Updates', url='https://t.me/hagadmansa'),
+                        InlineKeyboardButton('Support', url='https://t.me/hagadmansachat')
+                    ],
+                    [
+                        InlineKeyboardButton('Website', url='https://hagadmansa.com'),
+                        InlineKeyboardButton('Source', url='https://github.com/hagadmansa/FileStoreBot')
+                    ]
+                ]
+            )
+        )
+    elif len(message.command) == 2:
+        try:
+            link = f"https://t.me/{BOT_USERNAME}?start={message.command[1]}"
+            share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now,%20Join%20@Hagadmansa"
+            send = await message.reply_cached_media(
+                file_id=message.command[1],
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton('Share now', url=share)
+                        ]
+                    ]
+                )
+            )
+            media = replied.photo or replied.video or replied.voice or replied.document or replied.animation or replied.audio or replied.sticker 
+            await send.edit(media.file_name)
+        except:
+            await message.reply('The media you are trying to get is invalid.')       
+            
 @Client.on_message(filters.private & (filters.photo & filters.video & filters.voice & filters.document & filters.animation & filters.audio & filters.sticker))
 async def hagadmansa(bot, message):
-  hagadmansa = await message.reply("`Processing...`")
-  media = message.photo or message.video or message.voice or message.document or message.animation or message.audio or message.sticker 
-  link = f"https://t.me/{BOT_USERNAME}?start={media.file_id}"
-  share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now,%20Join%20@Hagadmansa"
-  await hagadmansa.edit(
-    text=f"Here is your link: {link}",
-    reply_markup=InlineKeyboardMarkup(
-        [[
-            InlineKeyboardButton('Share now', url=share)
-        ]])
-
+    hagadmansa = await message.reply("`Processing...`")
+    media = message.photo or message.video or message.voice or message.document or message.animation or message.audio or message.sticker 
+    link = f"https://t.me/{BOT_USERNAME}?start={media.file_id}"
+    share = f"https://t.me/share/url?url={link}&text=Click%20on%20link%20to%20get%20the%20file%20now,%20Join%20@Hagadmansa"
+    await hagadmansa.edit(
+        text=f"Here is your link: {link}",
+        reply_markup=InlineKeyboardMarkupp
+            [
+                [
+                    InlineKeyboardButton('Share now', url=share)
+                ]
+            ]
+        )
+    )
+    
 Bot.run()
